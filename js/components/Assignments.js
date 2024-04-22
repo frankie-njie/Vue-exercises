085d3c390a5640c0bc3684ff4d7a4a23bd4e25ba
frankie-njie <njie_frankie@yahoo.com>
2024-04-22 19:07:26 +0100

    using forms and tags

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,28 +1,27 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from './AssignmentCreate.js';
 
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
   /*html*/
   template: `
     <section class="space-y-6">
       <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
       <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-      <form @submit.prevent="add">
-        <div class="border border-gray-600 text-black">
-          <input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
-          <button type="submit" class="bg-white p-2 border-l">Add</button>
-        </div>
-      </form>
+      <assignment-create @add="add"></assignment-create>
+    
+
+
     </section>
     `,
 
   data() {
     return {
       assignments: [
-        { name: "Finish project", complete: false, id: 1 },
-        { name: "Read Chapter 4", complete: false, id: 2 },
-        { name: "Turn in Homeowork", complete: false, id: 3 },
+        { name: "Finish project", complete: false, id: 1, tag: "math" },
+        { name: "Read Chapter 4", complete: false, id: 2, tag: "science" },
+        { name: "Turn in Homeowork", complete: false, id: 3, tag: "math" },
       ],
 
       newAssignment : ''
@@ -39,9 +38,9 @@ export default {
   },
 
   methods: {
-    add() { 
+    add(name) { 
       this.assignments.push({
-        name: this.newAssignment,
+        name: name,
         completed: false,
         id: this.assignments.length + 1
       });
