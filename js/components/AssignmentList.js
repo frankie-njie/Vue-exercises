7b662770f0e00ab102cfd306e245ba215243477e
frankie-njie <njie_frankie@yahoo.com>
2024-04-22 20:08:27 +0100

    added tags component

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,7 +1,8 @@
 import Assignment from "./Assignment.js ";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-  components: { Assignment },
+  components: { Assignment, AssignmentTags },
   /*html*/
   template: `
     <!-- Incompleted Assignments -->
@@ -11,17 +12,13 @@ export default {
       <span> ({{ assignments.length }})</span>
     </h2>
 
-    <div class="flex gap-2">
-      <button 
-        @click="currentTag = tag"
-        v-for="tag in tags" 
-        class="border rounded px-1 py text-xs"
-        :class="{
-          'border-blue-500 text-blue-500' : tag === currentTag
-        }">
-        {{ tag }}
-      </button>
-    </div>
+    <assignment-tags 
+      :initial-tags='assignments.map(a => a.tag)'
+      :current-tag = "currentTag"
+      @change="currentTag = $event">
+      
+    </assignment-tags>
+    
     <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
       <assignment 
         v-for="assignment in filteredAssignments" 
@@ -49,10 +46,6 @@ export default {
         return this.assignments
       }
       return this.assignments.filter(a => a.tag === this.currentTag)
-    },
-
-    tags() {
-      return ['all', ...new Set(this.assignments.map((a) => a.tag))];
     },
   },
 };
